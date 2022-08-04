d9be9a42405b4347bde2fbdc52e7b46ea463b802
Edwards Moses <edwardsmoses3@gmail.com>
2022-08-04 10:21:26 +0100

    Setup the route to create the braintree payment transaction.
    Tested using the fake-valid-nonce

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,60 @@
 const express = require('express')
 const path = require('path');
+const braintree = require("braintree");
+const bodyParser = require('body-parser');
+
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env' });
+
 const app = express()
 const port = 3000
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set your secret key. Remember to switch to your live secret key in production.
+const gateway = new braintree.BraintreeGateway({
+  environment: braintree.Environment.Sandbox,
+  merchantId: process.env.BRAINTREE_MERCHANT_ID,
+  publicKey: process.env.BRAINTREE_PUBLIC_KEY,
+  privateKey: process.env.BRAINTREE_PRIVATE_KEY,
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/braintree', function(req, res) {
-    res.sendFile(path.join(__dirname, 'braintree.html'));
- });
+app.get('/braintree', function (req, res) {
+  res.sendFile(path.join(__dirname, 'braintree.html'));
+});
+
+app.post('/createPaymentTransaction', async (req, res) => {
+  const { body } = req;
+
+  try {
+
+    //create a transaction 
+    const result = await gateway.transaction.sale({
+      amount: body.amount,
+      paymentMethodNonce: body.nonce,
+      options: {
+        submitForSettlement: true
+      }
+    });
+
+    res.status(200).json({
+      isPaymentSuccessful: result.success,
+      errorText: result.transaction?.processorResponseText || "",
+    });
+
+  } catch (error) {
+    console.log("Error in creating transaction ", error);
+    res.status(400).json({
+      isPaymentSuccessful: false, errorText: "Error in creating the payment transaction" + error
+    });
+
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
